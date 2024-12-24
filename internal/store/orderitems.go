571f5caf3e558fe8f433622b0dfc51b19a0bedc6
TechmoNoway <cuty0406@outlook.com>
2024-12-24 22:51:26 +0700

    22: Change properties for order item model

--- internal/store/orderitems.go
+++ internal/store/orderitems.go
@@ -41,3 +41,46 @@ func (s *OrderItemStore) Create(ctx context.Context, orderItem *OrderItem) error
 
 	return nil
 }
+
+func (s *OrderItemStore) GetAll(ctx context.Context) ([]OrderItem, error) {
+	query := `
+		SELECT id, order_id, product_id, quantity, price, created_at
+		FROM order_items
+
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	rows.Close()
+
+	var orderItemList []OrderItem
+
+	for rows.Next() {
+		var orderItem OrderItem
+
+		err := rows.Scan(
+			&orderItem.ID,
+			&orderItem.OrderID,
+			&orderItem.ProductID,
+			&orderItem.Quantity,
+			&orderItem.Price,
+			&orderItem.CreatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		orderItemList = append(orderItemList, orderItem)
+
+	}
+
+	return orderItemList, err
+}
