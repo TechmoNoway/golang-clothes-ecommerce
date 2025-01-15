6a35a3263725f530bde4c4f5506b47d43880b6b1
TechmoNoway <cuty0406@outlook.com>
2025-01-15 23:12:51 +0700

    30: Update sign in form

--- web/src/_auth/forms/SignInForm.tsx
+++ web/src/_auth/forms/SignInForm.tsx
@@ -1,6 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 const SignInForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="flex h-screen rounded-lg shadow-lg">
       <div className="flex flex-col justify-center items-center w-[400px] md:h-full lg:h-full md:w-1/2 lg:w-1/2 p-10 bg-white">
@@ -59,7 +82,7 @@ const SignInForm = () => {
 
           {/* Sign-Up Link */}
           <p className="mt-4 text-sm text-gray-500 text-center">
-            I don't have an account?{" "}
+            I don't have an account?
             <a href="#" className="text-orange-500 hover:underline">
               Sign up
             </a>
@@ -76,6 +99,30 @@ const SignInForm = () => {
           />
         </div>
       </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
